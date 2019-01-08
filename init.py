cf72fa89eb62fe02a232b60468e7e1925f5a7200
Pavan Kumar <pavan.kumar@mahoujas.com>
2019-01-08 21:13:39 +0530

    Changed from YAML to JSON.

--- init.py
+++ init.py
@@ -2,30 +2,35 @@ import os.path
 
 
 def init():
-    init_yaml = '''---
-servers:
-  one:
-    host: 1.2.3.4
-    username: root
-app:
-  name: app
-  path: "./"
-  servers:
-    one: {}
-  buildOptions:
-    serverOnly: true
-  env:
-    ROOT_URL: http://app.com
-    MONGO_URL: mongodb://mongodb/meteor
-    MONGO_OPLOG_URL: mongodb://mongodb/local
-  docker:
-    image: abernix/meteord:node-8.4.0-base
-  enableUploadProgressBar: true
+    init_json = '''{
+   "servers":{
+      "one":{
+         "host":"1.2.3.4",
+         "username":"root",
+         "pem":"~/.ssh/id_rsa",
+         "env": {
+            
+         }
+      }
+   },
+   "app":{
+      "name":"abc",
+      "django_app": "",
+      "path":"../",
+      "docker":{
+         "image":"abernix/meteord:base"
+      },
+      "env":{
+         "ROOT_URL":"http://app.com",
+         "MONGO_URL":"mongodb://localhost/meteor"
+      }
+   }
+}
 '''
-    filename = 'django_up.yaml'
+    filename = 'django_up.json'
 
     if os.path.isfile(filename):
-        print('File django_up.yaml already exists.')
+        print('File {0} already exists.'.format(filename))
 
     with open(filename, 'w') as the_file:
-        the_file.write(init_yaml)
+        the_file.write(init_json)
