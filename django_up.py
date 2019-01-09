3675a3aa18678190917fee805953fa0631097b5a
Pavan Kumar <pavan.kumar@mahoujas.com>
2019-01-09 21:20:56 +0530

    Proper structuring of args parse

--- django_up.py
+++ django_up.py
@@ -7,7 +7,17 @@ from logs import logs
 
 def main():
     parser = ArgumentParser(description='Django up: Deployment tool for django.')
-    parser.add_argument('action', choices=['init', 'setup', 'deploy', 'logs'])
+    action_parser = parser.add_subparsers(
+        title='action',
+        description='Specify action to be performed by django_up',
+        dest='action',
+        required=True
+    )
+    action_parser.add_parser('init', help='Create settings files which will be used to deploy')
+    action_parser.add_parser('setup', help='Installs docker in the servers')
+    action_parser.add_parser('deploy', help='Deploys the django app to servers')
+    action_parser.add_parser('logs', help='Show logs of all docker containers')
+
     args = parser.parse_args()
     globals()[args.action]()
 
