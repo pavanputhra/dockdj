2012a5e560531fb8f020c902bc47a6ec9dacee15
Pavan Kumar <pavan.kumar@mahoujas.com>
2019-01-09 14:14:03 +0530

    Server logs command

--- django_up.py
+++ django_up.py
@@ -2,11 +2,12 @@ from argparse import ArgumentParser
 from init import init
 from setup import setup
 from deploy import deploy
+from logs import logs
 
 
 def main():
     parser = ArgumentParser(description='Django up: Deployment tool for django.')
-    parser.add_argument('action', choices=['init', 'setup', 'deploy'])
+    parser.add_argument('action', choices=['init', 'setup', 'deploy', 'logs'])
     args = parser.parse_args()
     globals()[args.action]()
 
