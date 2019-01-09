a6d6eb35b683f346a6644399a524777be3ac7edf
Pavan Kumar <pavan.kumar@mahoujas.com>
2019-01-09 13:58:55 +0530

    Help text added for yaml

--- init.py
+++ init.py
@@ -5,20 +5,26 @@ from du_settings import CONFIG_FILE, SETTINGS_PY_FILE
 INIT_YAML = '''---
 servers:
 - host: 1.2.3.4
+  # must be sudo without password
   username: root
   pem: "~/.ssh/id_rsa"
-  env: {}
+  # optional
+  env:
+    PER_SERVER: abc
 app:
+  # Any name used by script to name docker
   name: abc
+  # Django app dir which contains wsgi.py and settings.py
   django_app: 'hello_world'
   requirements_file: 'requirements.txt'
   port: 80
+  # Django app dir which contains wsgi.py and settings.py
   path: "/path/to/project"
   docker:
     image: python:3.7
   env:
-    FOO: http://app.com
-    BAR: mongodb://localhost/meteor
+    FOO: foo
+    BAR: bar
 '''
 
 
