814b261848967b3bcc36398c8b3fbb21ac7010a8
Pavan Kumar <pavan.kumar@mahoujas.com>
2019-02-16 22:08:09 +0530

    Install docker compose

--- dockdj/setup.py
+++ dockdj/setup.py
@@ -21,20 +21,33 @@ def setup(verbose=False):
                 connect_kwargs={'key_filename': server['pem']}) as cnx:
             try:
                 cnx.run('docker -v', hide=hide)
-                print('Docker already present')
+                cnx.run('docker-compose --version', hide=hide)
+                print('Docker & Docker compose already present')
             except exceptions.UnexpectedExit:
-                print('Docker not present')
+                print('Docker or docker compose not present')
                 install_docker(cnx, hide=hide)
 
 
 def install_docker(cnx, hide=True):
-    print('Installing docker')
-    cnx.run('sudo apt-get update', hide=hide)
-    cnx.run('sudo apt-get install unzip apt-transport-https ca-certificates curl software-properties-common -y',
-            hide=hide)
-    cnx.run('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -', hide=hide)
-    cnx.run('add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
-            hide=hide)
-    cnx.run('sudo apt-get update', hide=hide)
-    cnx.run('sudo apt-get install docker-ce -y', hide=hide)
-    print('Completed')
+    try:
+        cnx.run('docker -v', hide=hide)
+    except exceptions.UnexpectedExit:
+        print('Installing docker')
+        cnx.run('sudo apt-get update', hide=hide)
+        cnx.run('sudo apt-get install unzip apt-transport-https ca-certificates curl software-properties-common -y',
+                hide=hide)
+        cnx.run('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -', hide=hide)
+        cnx.run('add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"',
+                hide=hide)
+        cnx.run('sudo apt-get update', hide=hide)
+        cnx.run('sudo apt-get install docker-ce -y', hide=hide)
+        print('Docker installed')
+
+    try:
+        cnx.run('docker-compose --version', hide=hide)
+    except exceptions.UnexpectedExit:
+        print('Installing docker compose')
+        cnx.run('sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose', hide=hide)
+        cnx.run('sudo chmod +x /usr/local/bin/docker-compose',
+                hide=hide)
+        print('Docker compose installed')
