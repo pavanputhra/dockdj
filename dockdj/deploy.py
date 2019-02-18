c7c974b0350e3824d4baf2bc0bee84bd18540464
Pavan Kumar <pavan.kumar@mahoujas.com>
2019-02-18 17:41:30 +0530

    Working docker-compose up

--- dockdj/deploy.py
+++ dockdj/deploy.py
@@ -1,10 +1,13 @@
-from typing import Dict
+import io
 import os
+
+import jinja2
+import yaml
+
 from dockdj.util import read_config_files
 from fabric import Connection
 from invoke import exceptions
 import shutil
-from runpy import run_path
 from importlib import resources
 from string import Template
 
@@ -27,14 +30,15 @@ def manage(args='', verbose=False):
             user=server['username'],
             connect_kwargs={'key_filename': server['pem']}) as cnx:
         try:
-            build_docker_image(cnx, config_yaml, settings_py, hide)
-            server_env_opts = prepare_server_env_cmd_args(server)
-            cmd_args = ' '.join(args)
-            manage_cmd = f'python manage.py {cmd_args}'
-            print(manage_cmd)
-            cnx.run(
-                f'docker run {server_env_opts} --entrypoint "/bin/bash" {app_name} -c "{manage_cmd}"')
-            cnx.run(f'rm -Rf {server_dir}', hide=hide)
+            pass
+            # build_docker_image(cnx, config_yaml, settings_py, hide)
+            # server_env_opts = prepare_server_env_cmd_args(server)
+            # cmd_args = ' '.join(args)
+            # manage_cmd = f'python manage.py {cmd_args}'
+            # print(manage_cmd)
+            # cnx.run(
+            #     f'docker run {server_env_opts} --entrypoint "/bin/bash" {app_name} -c "{manage_cmd}"')
+            # cnx.run(f'rm -Rf {server_dir}', hide=hide)
         except exceptions.UnexpectedExit:
             print('Some exception')
 
@@ -56,45 +60,23 @@ def deploy(verbose=False):
         with Connection(
                 host=server['host'],
                 user=server['username'],
-                connect_kwargs={ 'key_filename': server['pem']}) as cnx:
+                connect_kwargs={'key_filename': server['pem']}) as cnx:
             try:
-                build_docker_image(cnx, config_yaml, settings_py, hide)
+                prepare_dir_structs(cnx, config_yaml, settings_py, hide)
                 run_docker_app(cnx, config_yaml, server, hide)
-            except exceptions.UnexpectedExit:
+
+            except exceptions.UnexpectedExit as e:
                 print('Some exception')
+                print(e)
 
     os.remove(f'{app_name}.zip')
 
 
-def run_docker_app(cnx, config_yaml, server, hide):
-    app_name = config_yaml["app"]["name"]
-    app_port = config_yaml['app']['port']
-    server_env_opts = prepare_server_env_cmd_args(server)
-
-    try:
-        cnx.run(f'docker stop {app_name}', hide=hide)
-        cnx.run(f'docker rm {app_name}', hide=hide)
-    except exceptions.UnexpectedExit:
-        pass
-    print('Trying to run app in docker')
-    cnx.run(f'docker run -d -p {app_port}:80 {server_env_opts} --name {app_name} {app_name}', hide=hide)
-    cnx.run(f'rm -Rf {server_dir}', hide=hide)
-    print('App running successfully')
-
-
-def prepare_server_env_cmd_args(server):
-    server_env_opts = ''
-    server_env = server['env']
-    if server_env is not None:
-        for k, v in server_env.items():
-            server_env_opts += f'-e {k}={v} '
-    return server_env_opts
-
-
-def build_docker_image(cnx, config_yaml, settings_py, hide):
+def prepare_dir_structs(cnx, config_yaml, settings_py, hide):
     app_name = config_yaml["app"]["name"]
     unzip_dir_path = f'{server_dir}/{app_name}'
     zip_file_path = f'{server_dir}/{app_name}.zip'
+    settings_path_rel = config_yaml["app"]["settings"]
 
     print('Uploading django project to server...')
     cnx.run(f'mkdir -p {server_dir}', hide=hide)
@@ -103,36 +85,49 @@ def build_docker_image(cnx, config_yaml, settings_py, hide):
     print('Upload complete')
 
     print('Preparing files...')
-    cnx.run(f'unzip {zip_file_path} -d {unzip_dir_path}', hide=hide)
 
-    append_settings_py_cmd = create_settings_cmd(config_yaml, settings_py)
+    cnx.run(f'mkdir -p {unzip_dir_path}/app', hide=hide)
+    cnx.run(f'unzip {zip_file_path} -d {unzip_dir_path}/app', hide=hide)
+    append_settings_py_cmd = create_settings_cmd(f'{unzip_dir_path}/app/{settings_path_rel}', settings_py)
     cnx.run(append_settings_py_cmd, hide=hide)
-
-    create_nginx_cmd = create_nginx_site_file(config_yaml)
-    cnx.run(create_nginx_cmd, hide=hide)
 
     docker_file_cmd = create_dock_file_cmd(config_yaml)
     cnx.run(docker_file_cmd, hide=hide)
+    with cnx.cd(f'{unzip_dir_path}/app'):
+        cnx.run(f'docker build -t {app_name} .', hide=hide)
 
-    print('Building docker image...')
-    cnx.run(f'docker build -t {app_name} {unzip_dir_path}', hide=hide)
+    cnx.run(f'mkdir -p {unzip_dir_path}/nginx/static')
+    cnx.run(f'docker run --volume {unzip_dir_path}/nginx/static:/static --workdir /app {app_name} python manage.py collectstatic --noinput')
+
+    create_nginx_cmd = create_nginx_site_file(config_yaml)
+    cnx.run(create_nginx_cmd, hide=hide)
+    nginx_docker = resources.read_text("dockdj", "nginx_docker.txt")
+    nginx_docker_io = io.StringIO(nginx_docker)
+    nginx_docker_io.name = 'Dockerfile'
+    cnx.put(nginx_docker_io, f'{server_dir}/{app_name}/nginx/')
+    with cnx.cd(f'{unzip_dir_path}/nginx'):
+        cnx.run(f'docker build -t {app_name}_nginx .', hide=hide)
+
+    compose_dict = create_compose_dict(config_yaml)
+    compose_string = yaml.dump(compose_dict, default_flow_style=False)
+    compose_file = io.StringIO(compose_string)
+    compose_file.name = 'docker-compose.yaml'
+    cnx.put(compose_file, unzip_dir_path)
 
 
-def create_settings_cmd(config_yaml, settings_py):
+def run_docker_app(cnx, config_yaml, server, hide):
     app_name = config_yaml["app"]["name"]
-    django_app = config_yaml['app']['django_app']
-    django_path = config_yaml['app']['path']
 
-    settings: Dict = run_path('./settings.py')
-    if 'STATIC_URL' in settings:
-        static_url = settings.get('STATIC_URL')
-    else:
-        settings: Dict = run_path(f'{django_path}/{django_app}/settings.py')
-        static_url = settings.get('STATIC_URL')
+    with cnx.cd(f'{server_dir}/{app_name}'):
+        cnx.run('docker-compose up -d')
+    print('App running successfully')
 
-    append_settings_py_cmd = f'''cat << EOF >> {server_dir}/{app_name}/{django_app}/settings.py
+
+def create_settings_cmd(settings_path, settings_py):
+
+    append_settings_py_cmd = f'''cat << EOF >> {settings_path}
 {settings_py}
-STATIC_ROOT = '/static_files{static_url}'
+STATIC_ROOT = '../static'
 EOF
  '''
     return append_settings_py_cmd
@@ -140,8 +135,13 @@ EOF
 
 def create_nginx_site_file(config_yaml):
     app_name = config_yaml["app"]["name"]
-    nginx_cmd = f'cat <<-"EOF" > {server_dir}/{app_name}/default\n'
-    nginx_config = resources.read_text("dockdj", "nginx.txt")
+    wsgi = config_yaml["app"].get('wsgi', None)
+    asgi = config_yaml["app"].get('asgi', None)
+    asgi_paths = config_yaml["app"]['asgi'].get('paths', []) if asgi else []
+    nginx_cmd = f'cat <<-"EOF" > {server_dir}/{app_name}/nginx/default\n'
+    nginx_config_temp = resources.read_text("dockdj", "nginx.jinja")
+    nginx_template = jinja2.Template(nginx_config_temp)
+    nginx_config = nginx_template.render(wsgi=wsgi, asgi=asgi, asgi_paths=asgi_paths)
     nginx_cmd += f'{nginx_config}\nEOF\n'
     return nginx_cmd
 
@@ -149,34 +149,79 @@ def create_nginx_site_file(config_yaml):
 def create_dock_file_cmd(config_yaml):
     docker_image = config_yaml['app']['docker']['image']
     req_file = config_yaml['app']['requirements_file']
-    dj_app = config_yaml['app']['django_app']
     app_name = config_yaml['app']['name']
-    envs_string = ''
-    for k, v in config_yaml['app']['env'].items():
-        envs_string += f'ENV {k} {v}\n'
 
-    
-    is_asgi = config_yaml['app']['asgi']
-    if is_asgi:
-        server = 'daphne'
-        server_command = f'daphne -b 0.0.0.0 -p 8000 {dj_app}.asgi:application'
-    else:
-        server = 'gunicorn'
-        server_command = f'gunicorn {dj_app}.wsgi -b 0.0.0.0:8000'
-
-    
     temp_data = {
         'docker_image': docker_image,
         'req_file': req_file,
-        'envs_string': envs_string,
-        'dj_app': dj_app,
-        'server': server,
-        'server_command': server_command
     }
 
     docker_template = resources.read_text("dockdj", "docker.txt")
     temp = Template(docker_template)
     docker_file_data = temp.substitute(temp_data)
 
-    docker_file_cmd = f'cat << EOF > {server_dir}/{app_name}/Dockerfile\n{docker_file_data}\nEOF'
+    docker_file_cmd = f'cat << EOF > {server_dir}/{app_name}/app/Dockerfile\n{docker_file_data}\nEOF'
     return docker_file_cmd
+
+
+def create_compose_dict(config_yaml):
+    app_name = config_yaml['app']['name']
+    compose_dict = {
+        'version': '3',
+        'services': {
+            'nginx': {
+                'image': f'{app_name}_nginx',
+                'depends_on': [],
+                'ports': ["8000:80"]
+            }
+        }
+    }
+
+    wsgi = config_yaml['app'].get('wsgi', None)
+    asgi = config_yaml['app'].get('asgi', None)
+    celery = config_yaml['app'].get('celery', None)
+
+    if wsgi:
+        wsgi_app = wsgi['app']
+        compose_dict['services']['wsgi'] = {
+            'image': app_name,
+            'command': f'gunicorn {wsgi_app} -b 0.0.0.0:80'
+        }
+
+        compose_dict['services']['nginx']['depends_on'].append('wsgi')
+
+    if asgi:
+        asgi_app = asgi['app']
+        compose_dict['services']['asgi'] = {
+            'image': app_name,
+            'command': f'daphne -b 0.0.0.0 -p 80 {asgi_app}'
+        }
+
+        compose_dict['services']['nginx']['depends_on'].append('asgi')
+
+    if celery:
+        celery_app = celery['app']
+        compose_dict['services']['celery'] = {
+            'image': app_name,
+            'command': f'celery worker -A {celery_app} -l info'
+        }
+
+        compose_dict['services']['nginx']['depends_on'].append('celery')
+
+    # append service from yaml
+    services = config_yaml['compose'].get('services', None)
+    if services:
+        service_names = services.keys()
+        for name in service_names:
+
+            if name in ['wsgi', 'asgi', 'nginx', 'celery']:
+                temp_image = compose_dict['services'][name]['image']
+                new_dict = services.get(name, {})
+                compose_dict['services'][name].update(new_dict)
+                compose_dict['services'][name]['image'] = temp_image
+                continue
+
+            compose_dict['services'][name] = services.get(name)
+
+    return compose_dict
+
